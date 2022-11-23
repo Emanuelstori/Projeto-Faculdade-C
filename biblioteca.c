05146b4720802f2d3c3a97443284b7714e5db10e
Emanuel Stori de Lara <63805292+Emanuelstori@users.noreply.github.com>
2022-11-23 15:04:48 -0300

    Update
    update

--- biblioteca.c
+++ biblioteca.c
@@ -16,6 +16,8 @@ Aluno *criaAluno()
     Aluno *novo;
 
     novo = malloc(sizeof(Aluno));
+    novo->matricula = -1;
+    novo->lista_disciplinas = NULL;
     novo->prox = NULL;
 
     return novo;
@@ -38,15 +40,37 @@ void imprimeAluno(Aluno *cabeca)
     if(cabeca == NULL)
         return;
     else
-    {
-        printf("%d\n", cabeca->matricula);
-        puts(cabeca->nome);
-        puts(cabeca->curso);
-        printf("$d\n", cabeca->lista_disciplinas->nome);//cabeca->lista_disciplinas
-        puts(cabeca->lista_disciplinas->nome);
-    }
+        {
+            if(cabeca->matricula != -1)
+                {
+                    printf("%d\n", cabeca->matricula);
+                    puts(cabeca->nome);
+                    puts(cabeca->curso);
+                    imprimeDisciplina(cabeca->lista_disciplinas);
+                }
+            else
+                return;
+        }
     imprimeAluno(cabeca->prox);
 };
+
+void imprimeDisciplina(Disciplina *cabeca)
+{
+    if(cabeca == NULL)
+        return;
+    else
+        {
+            if(cabeca->codigo != -1)
+                {
+                    printf("a\n");
+                    printf("%d\n", cabeca->codigo);
+                    puts(cabeca->nome);
+                }
+            else
+                return;
+        }
+    imprimeDisciplina(cabeca->prox);
+}
 
 Aluno *buscaAluno(Aluno *cabeca, int x)
 {
@@ -58,14 +82,33 @@ Aluno *buscaAluno(Aluno *cabeca, int x)
         return buscaAluno(cabeca->prox, x);
 }
 
+
 Disciplina *criaDisciplina()
 {
     Disciplina *novo;
 
     novo = malloc(sizeof(Disciplina));
+    novo->codigo = -1;
     novo->prox = NULL;
 
     return novo;
+}
+
+void leDisciplina(Disciplina *x)
+{
+    char c;
+    int i;
+    i=0;
+    printf("digite o codigo da disciplina: ");
+    scanf("%d",&(x->codigo));
+    printf("digite o nome da disciplina: ");
+    getchar();
+    while((c=getchar())!='\n')
+        {
+            x->nome[i]=c;
+            i++;
+        }
+    x->nome[i]='\0';
 }
 
 Disciplina *insereDisciplina(Disciplina *cabeca,int x, char *nome)
